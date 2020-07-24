b9bde1293500f5a2cf3e013ac115e4280c886a77
BWDaniels97 <bwdaniels97@gmail.com>
2020-07-24 11:58:34 +0000

    added database

--- application/routes.py
+++ application/routes.py
@@ -4,27 +4,14 @@ from flask import render_template
 from application import app
 # define routes for / & /home, this function will be called when these are accessed
 
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
-
-
-
+from application.models import Posts
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return render_template('home.html', title='Home', posts=blogData)
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
